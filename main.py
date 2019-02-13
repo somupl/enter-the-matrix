62775cc3954409fe29e4dbede2f69c29e34f9f30
Chyld Medford <chyld.medford@gmail.com>
2019-02-13 12:23:58 -0800

    ready to go...

---
+++ main.py
@@ -0,0 +1,17 @@
+from matrix import Matrix
+
+m1 = Matrix.random(3, 4)
+
+# 1 cry
+# 2 goto 1
+# read in the matrix.txt
+# filter the 4x2 and 2x4 matrices into
+# different lists
+
+# add any 2 compatible matrices
+# multiply any 2 compatible matrices
+# scale any matrix
+# create a list of 5 3x3 matrices of random values
+# and add them together
+
+# multiply a matrix times a vector
---
+++ matrix.py
@@ -0,0 +1,13 @@
+# create a matrix
+# any dimension
+# multiply 2 matrices
+# add 2 matrices
+# scale matrix
+# create a random mxn matrix
+# with values between 0 and 1
+
+
+class Matrix:
+    @staticmethod
+    def random(params):
+        pass
