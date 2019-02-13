69862ede0e32c057f33ea3210a6520bf98aaac5c
Palaniappan <Somu.Palaniappan@McKesson.com>
2019-02-13 17:05:09 -0600

    done

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from matrix import Matrix
-
-m1 = Matrix.random(3, 4)
-
-# 1 cry
-# 2 goto 1
 # read in the matrix.txt
 # filter the 4x2 and 2x4 matrices into
 # different lists
@@ -15,3 +9,50 @@ m1 = Matrix.random(3, 4)
 # and add them together
 
 # multiply a matrix times a vector
+
+
+from matrix import Matrix
+from vector import Vector
+from functools import reduce
+
+vectors = []
+matrix = []
+with open('matrix.txt') as f:
+    for line in f:
+        if len(line) > 1:
+            v = Vector(*tuple(map(int, line.strip().split(' '))))
+            vectors.append(v)
+        else:
+            m = Matrix(*vectors)
+            matrix.append(m)
+            vectors = []
+
+# different list
+mat_list1 = []
+mat_list2 = []
+for mat in matrix:
+    if mat.row == 4 and mat.col == 2:
+        mat_list1.append(mat)
+    else:
+        mat_list2.append(mat)
+
+# add ans
+add_m = mat_list1[0] + mat_list1[1]
+# print(mat_list1[0], mat_list1[1], add_m)
+
+# scale ans
+scale_m = mat_list1[0].scale(3)
+# print(mat_list1[0], scale_m)
+
+# multiply ans
+# print(add_m.transpose()) # needed transpose due to list
+print(mat_list2[0].mul(mat_list1[0]))
+
+# create a list of 5 3x3 matrices of random values and add them together
+m1 = Matrix.random(3, 4)
+# print(m1)
+
+mat_5 = [Matrix.random(3, 3) for _ in range(5)]
+empty_mat = Matrix(*[Vector(0, 0, 0), Vector(0, 0, 0), Vector(0, 0, 0)])
+final_mat = reduce(lambda acc, mat: acc + mat, mat_5, empty_mat)
+print(final_mat)
--- matrix.py
+++ matrix.py
@@ -6,8 +6,61 @@
 # create a random mxn matrix
 # with values between 0 and 1
 
+import random as r
+from vector import Vector
+
 
 class Matrix:
+
+    def __init__(self, *vect):
+        self.mat = vect
+        self.row = len(vect)
+        self.col = self.mat[0].len
+
+    def __str__(self):
+        msg1 = "This is a {} X {} matrix".format(self.row, self.col)
+        msg2 = [x.nums for x in self.mat]
+        return "{} \n {} \n".format(msg1, msg2)
+
     @staticmethod
-    def random(params):
-        pass
+    def random(row, col):
+        v1 = []  # list of numbers
+        vec = []
+        for _ in range(row):
+            for _ in range(col):
+                v1.append(r.random())
+            vec.append(Vector(*v1))
+            v1 = []
+        return Matrix(*vec)
+
+    def __add__(self, other):
+        return Matrix(*[c1 + c2 for c1, c2 in zip(self.mat, other.mat)])
+
+    def scale(self, scalar):
+        return Matrix(*[n.scale(scalar) for n in self.mat])
+
+    def transpose(self):
+        v = []
+        m = []
+        for col in range(self.col):
+            for row in self.mat:
+                v.append(row.nums[col])
+            m.append(Vector(*v))
+            v = []
+        return Matrix(*m)
+
+    def mul(self, other):
+        t_other = other.transpose()
+        print(self)
+        print(t_other)
+        v = []
+        m = []
+        for row in self.mat:
+            # print(row)
+            for col in t_other.mat:
+                # print(col)
+                v.append(row.dot(col))
+            vec = Vector(*v)
+            m.append(vec)
+            v = []
+        return Matrix(*m)
